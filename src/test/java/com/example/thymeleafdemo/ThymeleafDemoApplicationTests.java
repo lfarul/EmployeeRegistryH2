1bc236550dad1957a4c98e1ae6d16b34d88f0dcc
lfarul <LFarulewski@yahoo.com>
2019-06-10 20:29:31 +0200

    Update ThymeleafDemoApplicationTests.java

--- src/test/java/com/example/thymeleafdemo/ThymeleafDemoApplicationTests.java
+++ src/test/java/com/example/thymeleafdemo/ThymeleafDemoApplicationTests.java
@@ -1,60 +1,31 @@
 package com.example.thymeleafdemo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import static org.junit.Assert.assertEquals;
+import javax.transaction.Transactional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.example.thymeleafdemo.dao.EmployeeRepository;
 import com.example.thymeleafdemo.entity.Employee;
+import com.example.thymeleafdemo.service.EmployeeService;
 
 @RunWith(SpringRunner.class)
+@Transactional
 @SpringBootTest
 public class ThymeleafDemoApplicationTests {
 	
-	// load employee data
-	//private List <Employee> theEmployees;
-	
-	@PersistenceContext
-	EntityManager entityManager;
-	
 	@Autowired
-	EmployeeRepository employeeRepository;
-	
+	private EmployeeService employeeService;
+
 	@Test
 	public void contextLoads() {
 		
-		// create employees
+		Employee employee = new Employee(1,"Lukasz","Farulewski","85-796","Lukasz@yahoo.com");
+		employeeService.save(employee);
 		
-		//Employee lukasz = new Employee();
-		
-		//lukasz.setFirstName(" Mike");
-		//lukasz.setLastName("Jordan");
-		//lukasz.setEmail("Mike@yahoo.com");
-		//employeeRepository.saveAndFlush(lukasz);
-		
-		
-		// create the list
-		
-		//theEmployees = new ArrayList<>();
-		
-		// add employees to the list
-		
-		//theEmployees.add(lukasz);
+		Employee employee2 = employeeService.findById(1);
 
-		
-		//then
-		//assertThat(lukasz.getFirstName())
-		//.isEqualTo(lukasz.getFirstName());	
+        assertEquals("Lukasz", employee2.getFirstName());	
 	}
-
 }
